621cf5424a00427dfbd3a3609a020f9bd92752fe
huazhe.zhou.je <huazhe.zhou.je@hitachi.com>
2021-01-22 13:48:18 +0900

    v1.1

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,44 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+import { DataStore } from '@aws-amplify/datastore';
+import { User } from './models';
+import Vue from 'vue'
+import App from './App.vue'
+Amplify.configure(awsconfig)
+Vue.config.productionTip = false
 
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+ const models =  DataStore.query(User);
+        console.log("models"+models);
+        //this.models = models
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+		models:[]
+        }
+    },
+	mounted(){
+            //this.selectGood()
+    },
+           methods: {
+           async insert(){
+           await DataStore.save(
+            new User({
+		"name1": "Lorem ipsum dolor sit amet",
+		"age1": 1020
+	})
+        )
+           }
+
+}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -12,14 +12,6 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-methods: {
-  async createUser() {
-        await DataStore.save(
-            new User({
-		"name1": "Lorem ipsum dolor sit amet",
-		"age1": 1020
-	})
-        );
-Copy
-  }
-}
+
+ const models =  DataStore.query(User);
+        console.log("models"+models);
